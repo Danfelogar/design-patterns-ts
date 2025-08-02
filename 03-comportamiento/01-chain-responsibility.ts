de68f9916081c7cf3c725b9475714b64af4a82bf
Danfelogar <danypolo18@hotmail.com>
2025-08-02 16:15:02 -0500

    feat: adding solution of chain-responsability

--- 03-comportamiento/01-chain-responsibility.ts
+++ 03-comportamiento/01-chain-responsibility.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * ! Patron Chain of Responsibility
  * Es un patrón de diseño de comportamiento que te permite pasar solicitudes
@@ -9,3 +11,88 @@
  *
  * https://refactoring.guru/es/design-patterns/chain-of-responsibility
  */
+interface Handler {
+  setNext(handler: Handler): Handler;
+  handle(request: string): void;
+}
+
+abstract class BaseHandler implements Handler {
+  private nextHandler?: Handler;
+
+  setNext(handler: Handler): Handler {
+    this.nextHandler = handler;
+    return handler;
+  }
+
+  handle(request: string): void {
+    if (this.nextHandler) {
+      this.nextHandler.handle(request);
+    }
+  }
+}
+
+//Basic Support
+class BasicSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === "basic") {
+      console.log(`%c Basic Support: Resolving request`, COLORS.green);
+      return;
+    }
+    console.log(
+      `%cBasic Support: Passing request to next handler --> ${request}`,
+      COLORS.yellow
+    );
+    super.handle(request);
+  }
+}
+
+//Advanced Support
+class AdvancedSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === "advanced") {
+      console.log(`%c Advanced Support: Resolving request`, COLORS.green);
+      return;
+    }
+    console.log(
+      `%cAdvanced Support: Passing request to next handler --> ${request}`,
+      COLORS.yellow
+    );
+    super.handle(request);
+  }
+}
+
+//Expert Support
+class ExpertSupport extends BaseHandler {
+  override handle(request: string): void {
+    if (request === "expert") {
+      console.log(`%c Expert Support: Resolving request`, COLORS.green);
+      return;
+    }
+    console.log(
+      `%cExpert Support: there isn't anything to do with this request :( --> ${request}`,
+      COLORS.yellow
+    );
+  }
+}
+
+function main() {
+  const basicSupport = new BasicSupport();
+  const advancedSupport = new AdvancedSupport();
+  const expertSupport = new ExpertSupport();
+
+  basicSupport.setNext(advancedSupport).setNext(expertSupport);
+
+  console.log(`%c Requesting basic support`, COLORS.blue);
+  basicSupport.handle("basic");
+
+  console.log(`%c Requesting advanced support`, COLORS.blue);
+  basicSupport.handle("advanced");
+
+  console.log(`%c Requesting expert support`, COLORS.blue);
+  basicSupport.handle("expert");
+
+  console.log(`%c Requesting unknown support`, COLORS.blue);
+  basicSupport.handle("unknown");
+}
+
+main();
--- 03-comportamiento/01.2-chain-responsibility.ts
+++ 03-comportamiento/01.2-chain-responsibility.ts
@@ -8,7 +8,7 @@
  * * pero se sabe que se necesita procesar en una secuencia.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // 1. Interfaz Approver
 interface Approver {
@@ -32,11 +32,9 @@ abstract class BaseApprover implements Approver {
     if (this.nextApprover) {
       this.nextApprover.approveRequest(amount);
       return;
-    } 
-    
-    
-    console.log('Solicitud no pudo ser aprobada.');
-    
+    }
+
+    console.log("Solicitud no pudo ser aprobada.");
   }
 }
 
@@ -46,7 +44,18 @@ class Supervisor extends BaseApprover {
   // TODO: Implementar el método approveRequest si el monto es menor o igual a 1000
   // TODO: Si el monto es mayor a 1000, pasar la solicitud al siguiente aprobador
   override approveRequest(amount: number): void {
-    throw new Error('Method not implemented.');
+    if (amount <= 1000) {
+      console.log(
+        `%c Supervisor: Aprobando solicitud de compra de $${amount}`,
+        COLORS.green
+      );
+      return;
+    }
+    console.log(
+      `%cSupervisor: Pasando solicitud al siguiente aprobador --> $${amount}`,
+      COLORS.yellow
+    );
+    this.next(amount);
   }
 }
 
@@ -55,32 +64,53 @@ class Manager extends BaseApprover {
   // TODO: Si el monto es mayor a 5000, pasar la solicitud al siguiente aprobador
 
   override approveRequest(amount: number): void {
-    throw new Error('Method not implemented.');
+    if (amount <= 5000) {
+      console.log(
+        `%c Manager: Aprobando solicitud de compra de $${amount}`,
+        COLORS.green
+      );
+      return;
+    }
+    console.log(
+      `%cManager: Pasando solicitud al siguiente aprobador --> $${amount}`,
+      COLORS.yellow
+    );
+    this.next(amount);
   }
 }
 
 class Director extends BaseApprover {
   // TODO: Implementar el método approveRequest si el monto
+  override approveRequest(amount: number): void {
+    console.log(
+      `%c Director: Aprobando solicitud de compra de $${amount}`,
+      COLORS.green
+    );
+    // No hay siguiente aprobador, ya que el director aprueba todo
+  }
 }
 
 // 4. Código Cliente para probar la cadena de responsabilidad
 
 function main() {
+  //supervisor: =< 1000
   const supervisor = new Supervisor();
+  //Manager: =< 5000
   const manager = new Manager();
+  //Director allow everything
   const director = new Director();
 
   // Configurar la cadena de responsabilidad
   supervisor.setNext(manager).setNext(director);
 
   // Probar diferentes solicitudes de compra
-  console.log('Solicitud de compra de $500:');
+  console.log("Solicitud de compra de $500:");
   supervisor.approveRequest(500);
 
-  console.log('\nSolicitud de compra de $3000:');
+  console.log("\nSolicitud de compra de $3000:");
   supervisor.approveRequest(3000);
 
-  console.log('\nSolicitud de compra de $7000:');
+  console.log("\nSolicitud de compra de $7000:");
   supervisor.approveRequest(7000);
 }
 
